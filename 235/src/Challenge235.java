5ed18c24f1a47ba1398f6f11ca62b334652ed6f3
clarencechee <clarencechee94@gmail.com>
2016-11-18 03:39:00 +0800

    add frame + stuff

--- 235/src/Challenge235.java
+++ 235/src/Challenge235.java
@@ -1,4 +1,7 @@
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /*
  * reddit.com/r/dailyprogrammer/comments/3ntsni/20151007_challenge_235_intermediate_scoring_a/
@@ -13,32 +16,81 @@ public class Challenge235 {
         Scanner sc = new Scanner(System.in);
         
         while (true) {
-            BowlingScore score = new BowlingScore(sc.nextLine().trim());
+            ScoreSheet score = new ScoreSheet(sc.nextLine().trim());
             System.out.println(String.format(MESSAGE_SCORE_SHEET, score.getScoreSheet()));
             System.out.println(String.format(MESSAGE_SCORE_VALUE, score.getScore()));
         }
     }
 }
 
-class BowlingScore {
+class ScoreSheet {
     
-    private String scoreSheet;
-    private int scoreValue;
+    private String fullScoreSheet;
+    private List<Frame> frames;
+    private int totalScore;
     
-    public BowlingScore(String scoreSheet) {
-        this.scoreSheet = scoreSheet == null ? "" : scoreSheet;
-        score();
-    }
-    
-    private void score() {
-        
+    public ScoreSheet(String scoreSheet) {
+        this.fullScoreSheet = scoreSheet == null ? "" : scoreSheet;
+        determineFrames();
+        determineTotalScore();
     }
     
     public int getScore() {
-        return this.scoreValue;
+        return this.totalScore;
     }
     
     public String getScoreSheet() {
-        return scoreSheet;
+        return fullScoreSheet;
+    }
+    
+    private void determineTotalScore() {
+        this.totalScore = frames.stream().collect(Collectors.summingInt(f -> f.getFrameScore()));
+    }
+    
+    private void determineFrames() {
+        frames = Stream.of(fullScoreSheet.split("\\s+"))
+                .map(Frame::new)
+                .collect(Collectors.toList());
+    }
+}
+
+class Frame {
+    
+    private String frameText;
+    private FrameType frameType;
+    private int frameScore;
+    
+    private enum FrameType {
+        LAST, STRIKE, STANDARD
+    }
+    
+    public Frame(String frameValue) {
+        this.frameText = frameValue == null ? "" : frameValue;
+        determineFrameType();
+        determineFrameScore();
+    }
+    
+    public int getFrameScore() {
+        return frameScore;
+    }
+    
+    private void determineFrameScore() {
+        
+    }
+    
+    private void determineFrameType() {
+        switch (frameText.length()) {
+        case 1 :
+            frameType = FrameType.STRIKE;
+            return;
+        case 2 :
+            frameType = FrameType.STANDARD;
+            return;
+        case 3 :
+            frameType = FrameType.LAST;
+            return;
+        default :
+            return;
+        }
     }
 }
