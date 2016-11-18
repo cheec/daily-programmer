0543d5449e47ea1b952939be56417b40c1b6b078
clarencechee <clarencechee94@gmail.com>
2016-11-18 21:29:48 +0800

    skeleton

---
+++ 245/src/main/Challenge245.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * reddit.com/r/dailyprogrammer/comments/3x3hqa/20151216_challenge_245_intermediate_ggggggg_gggg/
+ */
+public class Challenge245 {
+    
+    public static void main(String[] args) {
+        
+    }
+}
